02257eec3b4d9261ce2877e4abdff806bbc29c56
Danielle <daniellexsousa@gmail.com>
2020-12-08 00:22:06 -0300

    new components

---
+++ course-manager/src/app/courses/course-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html'
+})
+
+export class CourseListComponent implements OnInit {
+
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name:'Angular Forms',
+                imageUrl: '/assets/images/forms.png',
+                price: 99.99,
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 4.5,
+                releaseDate: 'December 2, 2019'
+            },
+
+            {
+                id: 2,
+                name:'Angular HTTP',
+                imageUrl: '/assets/images/http.png',
+                price: 45.99,
+                code: 'LKL-1094',
+                duration: 80,
+                rating: 4,
+                releaseDate: 'November 4, 2019'
+            }
+        ]
+    }
+
+
+}
---
+++ course-manager/src/app/star/star.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnChanges } from '@angular/core';
+
+@Component ({
+    selector: 'app-star'
+})
+
+export class StarComponent implements OnChanges {
+
+    @Input()
+    rating: number = 0;
+
+    starWidth: number;
+
+    ngOnChanges(): void {
+        this.starWidth = this.rating * 94 / 5;
+    }
+}
