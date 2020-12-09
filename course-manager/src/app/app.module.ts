fb04d7e22cb9675517772674988c7f04dd15cd92
Danielle <daniellexsousa@gmail.com>
2020-12-09 12:23:41 -0300

    course info

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { Error404Component } from './404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 
 @NgModule({
@@ -18,21 +19,29 @@ import { Error404Component } from './404/error-404.component';
     StarComponent,
     ReplacePipe,
     NavBarComponent,
-    Error404Component
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       {
-        path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
         path: 'courses', component: CourseListComponent
       },
+      
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      
       {
         path: '**', component: Error404Component
-      }
+      },
+      
     ])
   ],
   providers: [],
