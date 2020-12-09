6889685b9b712f31cb99c8c89d136137ea952fdd
Danielle <daniellexsousa@gmail.com>
2020-12-09 11:26:15 -0300

    navbar

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -6,17 +6,29 @@ import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
