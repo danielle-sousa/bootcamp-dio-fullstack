222de9756adcf60bebec87baffe0ea44ab355d5d
Danielle <daniellexsousa@gmail.com>
2020-12-09 18:43:22 -0300

    forms

--- course-manager/src/app/courses/course-info.component.ts
+++ course-manager/src/app/courses/course-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CourseService } from './course.service';
+import { Course } from './course';
 
 @Component({
     templateUrl: './course-info.component.html'
@@ -7,11 +9,15 @@ import { ActivatedRoute } from '@angular/router';
 
 export class CourseInfoComponent implements OnInit {
     
-    courseId: number;
+    course: Course;
 
-    constructor(private activatedRoute: ActivatedRoute) { }
+    constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService) { }
     
     ngOnInit(): void {
-        this.courseId = +this.activatedRoute.snapshot.paramMap.get('id');
+        this.course = this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id'));
+    }
+
+    save(): void {
+        this.courseService.save(this.course);
     }
 }
